432ad874559f50e99a67ab2aae24e1876f7d74eb
Singana Sreenivasulu (RBEI/BSP2) <Sreenivasulu.Singana@in.bosch.com>
2018-10-26 12:13:05 +0530

    Form builder form added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  regForm = new FormGroup({
+    userName : new FormControl('sreeni'),
+    password : new FormControl(''),
+    confirmPassword : new FormControl(''),
+    address : new FormGroup({
+      city : new FormControl(''),
+      state : new FormControl('AP'),
+      postalCode : new FormControl('')
+    })
+  });
+
+  loadApiData(){
+    this.regForm.setValue({
+      userName : 'sreeni',
+      password : 'test',
+      confirmPassword : 'test',
+      address : {
+        city : 'City',
+        state : 'state',
+        postalCode : '981099'
+      }
+    });    
+  }
+
+  loadAdressData(){
+    this.regForm.patchValue({
+      address : {
+        city : 'City',
+        state : 'state',
+        postalCode : '981099'
+      }
+    });    
+  }
 }
